56b8d4c53d74e38ae3d6de7204e99817f80d323a
adii <abhishekdass08@gmail.com>
2017-12-09 02:39:42 -0500

    tempcomp update formcontrol

--- src/app/quiz/tempcomp/tempcomp.component.ts
+++ src/app/quiz/tempcomp/tempcomp.component.ts
@@ -5,12 +5,13 @@ import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angula
   selector: 'tempcomp',
   templateUrl: './tempcomp.component.html',
   styleUrls: ['./tempcomp.component.scss'],
-  inputs: ['headerSectionFormGroup'],
+  inputs: ['headerSectionFormGroup', 'ctrlName'],
   outputs: ['updatedFormGrpEvent']
 })
 export class TempcompComponent implements OnInit {
 
 	@Input() headerSectionFormGroup: FormGroup;
+	@Input() ctrlName: string;
 
 	public updatedFormGrpEvent = new EventEmitter<any>();
 
@@ -19,18 +20,24 @@ export class TempcompComponent implements OnInit {
 	constructor() { }
 
 	ngOnInit() {
-	}
+		// console.log('ctrlName >>> ',this.ctrlName);
+		this.control = new FormControl(this.ctrlName, Validators.required);
 
-	updateExisting(name:string) {
-
-
-		this.control = new FormControl(name, Validators.required);
-
-		this.headerSectionFormGroup.addControl(name, this.control);
+		this.headerSectionFormGroup.addControl(this.ctrlName, this.control);
 
 		this.updatedFormGrpEvent.emit(this.headerSectionFormGroup);
+	}
+
+	// updateExisting(name:string) {
+
+
+		// this.control = new FormControl(name, Validators.required);
+
+		// this.headerSectionFormGroup.addControl(name, this.control);
+
+		// this.updatedFormGrpEvent.emit(this.headerSectionFormGroup);
 
 		// console.log('updated form group > ',  this.headerSectionFormGroup);
-	}
+	// }
 
 }
