1f13ec186b6f88c2d76f1b2f52407fb0c8f6ee45
adii <abhishekdass08@gmail.com>
2017-12-09 04:03:03 -0500

    clean code

--- src/app/quiz/tempcomp/tempcomp.component.ts
+++ src/app/quiz/tempcomp/tempcomp.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, Input, ElementRef } from '@angular/core';
-import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'tempcomp',
@@ -12,38 +12,22 @@ export class TempcompComponent implements OnInit {
 	@Input() headerSectionFormGroup: FormGroup;
 	@Input() ctrlName: string;
 
-	public elRef: ElementRef;
-	// public updatedFormGrpEvent = new EventEmitter<any>();
 
 	public control: FormControl;
 
 	public cSelection: string;
 
-	constructor(private _elRef: ElementRef) { }
+	constructor() { }
 
 	ngOnInit() {
 
 		this.control = new FormControl(this.cSelection, Validators.required);
-
 		this.headerSectionFormGroup.addControl(this.ctrlName, this.control);
-
-		// this.updatedFormGrpEvent.emit(this.headerSectionFormGroup);
 	}
 
 	onChange($e) {
+
 		this.cSelection = $e.target.value;
 		this.control.setValue(this.cSelection);
 	}
-	// updateExisting(name:string) {
-
-
-		// this.control = new FormControl(name, Validators.required);
-
-		// this.headerSectionFormGroup.addControl(name, this.control);
-
-		// this.updatedFormGrpEvent.emit(this.headerSectionFormGroup);
-
-		// console.log('updated form group > ',  this.headerSectionFormGroup);
-	// }
-
 }
